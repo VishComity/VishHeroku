1360bf8bb51e79620e2915e976060f07557965c6
Vishal Patel <vishal@comitydesigns.com>
2016-12-08 14:56:53 -0500

    Updated oauth.js

--- oauth.js
+++ oauth.js
@@ -53,8 +53,9 @@ exports.oauth = function oauth(options) {
                 req.session.oauth = data;
                 state = req.session.oauth_state;
                 delete req.session.oauth_state;
+                console.log('oauth done - data ---> '+data);
                 console.log('oauth done - redirecting to '+state);
-        		res.redirect(state);
+        		//res.redirect(state);
               }
             }).on('error', function(e) {
     			  console.error(e);
