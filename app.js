0d7c4465e57cac92408952d2467cc07976bed3de
Vishal Patel <vishal@comitydesigns.com>
2016-12-12 14:24:05 -0500

    Updated

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-
-console.log('1');
+var nforce = require('nforce');
 var express = require('express');
-console.log('2');
-var oauth = require('./lib/oauth');
-console.log('3');
-var app = express();
-console.log('4');
 var port = process.env.PORT || 3000;
 
+var org = nforce.createConnection({
+	clientId: '3MVG9xOCXq4ID1uENR_n0vamNcV9ttUyOINbn6coRAQ4rj4QrMvdOoUozVXyqlXIZXQ0Yp1kgmpGRGXX0j1cB',
+	clientSecret: '5893789878304722858',
+	redirectUri: 'https://oauthss.herokuapp.com/oauthcallback',
+	apiVersion: 'v38.0',  // optional, defaults to current salesforce API version
+	environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+	mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
+});
+
+var app = express();
 
 // Require Routes js
 var routesHome = require('./routes/home');
@@ -20,13 +24,20 @@ app.use('/home', routesHome);
 app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
-	console.log('before redirectAuthURI');
-	oauth.redirectAuthURI(req, res, app);
+  res.redirect(org.getAuthUri());
 });
 
-app.get('/oauthcallback', function(req, res, app) {
-	console.log('before redirectAuthURI');
-	oauth.authenticate(req, res, app);
+app.get('/oauthcallback', function(req, res) {
+	org.authenticate({code: req.query.code}, function(err, resp){
+		if(!err) {
+			console.log('Access Token: ' + resp.access_token);
+			app.locals.oauthtoken = resp.access_token;
+			app.locals.lightningEndPointURI = "https://vishlightningss-dev-ed.lightning.force.com/";
+			res.redirect('/home');
+		} else {
+			console.log('Error: ' + err.message);
+		}
+	});
 });
 
 // Served Localhost
