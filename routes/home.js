67e2eb02ce195b55b473ff80e52a1e50c89a9416
Vishal Patel <vishal@comitydesigns.com>
2016-12-16 16:11:32 -0500

    Updated

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/home', function(req, res) {
     res.render('pages/home', {
         oauthtoken: req.app.locals.oauthtoken,
-        ouathLightningURL: req.app.locals.lightningEndPointURI,
+        ouathLightningURL: req.app.locals.lightningEndPointURI
     });
 });
 
