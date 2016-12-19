3439425dcfc2ebc826acbf74bfeaee907f4d0110
Vishal Patel <vishal@comitydesigns.com>
2016-12-19 07:58:00 -0500

    Updated

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ var app = express();
 
 // Require Routes js
 var routesHome = require('./routes/home');
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+app.use('/home', routesHome);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	oauth.redirectAuthURI(res);
+});
+
+app.get('/oauthcallback', function(req, res) {
+	oauth.authenticate(req, res, app);
+});
+
+// Served Localhost
+console.log('Served: http://localhost:' + port);
+app.listen(port);
+/*
+
+"use strict";
+var express = require('express');
+var oauth = require('./lib/oAuth/oauth');
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// Require Routes js
+var routesHome = require('./routes/home');
 //Require Routes js
 //var routesAdmin = require('./routes/admin');
 
@@ -38,3 +68,4 @@ app.get('/oauthcallback', function(req, res) {
 // Served Localhost
 console.log('Served: http://localhost:' + port);
 app.listen(port);
+*/
