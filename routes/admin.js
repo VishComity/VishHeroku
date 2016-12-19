cf8c01a91d12a1b3687169464e5f6f0cf8801f9e
Vishal Patel <vishal@comitydesigns.com>
2016-12-19 07:23:19 -0500

    Updated

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,8 @@ var router = express.Router();
 router.get('/admin', function(req, res) {
 	    res.render('pages/admin', {
 	        oauthtoken: req.app.locals.oauthtoken,
-	        ouathLightningURL: req.app.locals.lightningEndPointURI
+	        ouathLightningURL: req.app.locals.lightningEndPointURI,
+	        refreshtoken: req.app.locals.refreshToken
     });
 });
 
